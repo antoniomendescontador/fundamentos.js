3fa1c1f83f752828ff834f8eaf6bd86fd12f468a
Antonio Mendes <antonio.mendes.contador@gmail.com>
2020-05-18 15:07:51 -0400

    aritmeticos

---
+++ aritmeticos.js
@@ -0,0 +1,15 @@
+//operador pre-fix e unário
+//++d
+
+//operador pos-fix e unário
+//d++
+
+//operador in-fix e binário
+const [a, b, c, d] = [3, 5, 1, 15]
+const soma = a + b + c + d //infix e binário
+const subtracao = d - b
+const multiplicacao = b * c
+const divisao = d / a
+const modulo = a % 2
+
+console.log(soma,subtracao,multiplicacao,divisao,modulo)
